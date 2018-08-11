94e833e14de07b247e5c362c47b4763ade8c0e30
b-knight <knight.benjamin@gmail.com>
2018-08-11 10:40:35 -0700

    First commit

---
+++ Multicollinearity.R
@@ -0,0 +1,13 @@
+load("/home/bknight/Documents/Academics/R_Work/WV6_Data_R_v_2016_01_01.rdata")
+# Important in life: Religion	
+# For each of the following, indicate how important it is in your life. 
+# Would you say it is:  Religion	3		1##Very important 
+                                      # 2##Rather important 
+                                      # 3##Not very important 
+                                      # 4##Not at all important 
+                                      # -5##BH: Missing; 
+                                      # RU:Inappropriate response{Inappropriate} 
+                                      # -4##Not asked in survey 
+                                      # -3##Not applicable 
+                                      # -2##No answer 
+                                      # -1##Don't know 
